c7190890a89d9105db0c21e859dc573c0b71bd90
jordilg13 <jordillopis00@gmail.com>
2020-01-08 13:42:25 +0100

    frontend added and dockerized

--- django-realworld-example-app/conduit/settings.py
+++ django-realworld-example-app/conduit/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+    "127.0.0.1",
+    "localhost"
+]
 
 
 # Application definition
@@ -130,9 +134,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = (
-    '0.0.0.0:4000',
-    'localhost:4000',
+    '0.0.0.0:8000',
+    'localhost:8000',
 )
+
 
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
